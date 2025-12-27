24caa82abbd5515ea32db1c48592279907cf8eff
nguoingoaihanhtinh <22520688@gm.uit.edu.vn>
2025-12-27 16:17:18 +0700

    feat: docker config

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import LandingPage from "../components/LandingPage";
+import LoginPage from "../pages/LoginPage";
 
 export const Route = createFileRoute("/")({
-  component: LandingPage,
+  component: LoginPage,
 });
